323472be59ae4ba182c79449894fb3466c5d3a0e
pratap86 <narayanpratap86@gmail.com>
2021-01-01 21:42:04 +0530

    add more assertions from Hamcrest Matcher

--- src/test/java/com/pratap/designpatterns/creational/builder/BuilderTest.java
+++ src/test/java/com/pratap/designpatterns/creational/builder/BuilderTest.java
@@ -1,5 +1,8 @@
 package com.pratap.designpatterns.creational.builder;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -21,12 +24,41 @@ class BuilderTest {
 		
 	}
 	
+	@Test
+	void testBuilder_By_Name_Only() {
+
+		user = createUser();
+		UserDTO userDTO = directBuildByUserName(new UserWebDTOBuilder(), user);
+		assertNotNull(userDTO);
+		assertEquals("Pratap Narayan", userDTO.getName());
+		
+	}
+	
+	@Test
+	void testBuilder_By_User_Address_Only() {
+
+		user = createUser();
+		UserDTO userDTO = directBuildByUserAddress(new UserWebDTOBuilder(), user);
+		assertNotNull(userDTO);
+		assertThat(userDTO.getAddress(), containsString("Noida"));
+		assertThat(userDTO.getAddress(), endsWith("260230"));
+	}
+	
 	/**
 	 * This method serves the role of director in builder pattern.
 	 */
 	private static UserDTO directBuild(UserDTOBuilder builder, User user) {
 		return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName())
 				.withBirthday(user.getBirthday()).withAddress(user.getAddress()).build();
+	}
+	
+	private static UserDTO directBuildByUserName(UserDTOBuilder builder, User user) {
+		return builder.withFirstName(user.getFirstName())
+				.withLastName(user.getLastName()).build();
+	}
+	
+	private static UserDTO directBuildByUserAddress(UserDTOBuilder builder, User user) {
+		return builder.withAddress(user.getAddress()).build();
 	}
 
 	/**
