062928655789bd0a695b9af10d6b8aa2db986931
pratap86 <narayanpratap86@gmail.com>
2021-02-13 19:41:52 +0530

    proxy dp feature

---
+++ src/main/java/com/pratap/designpatterns/structural/proxy/dynamic/Human.java
@@ -0,0 +1,7 @@
+package com.pratap.designpatterns.structural.proxy.dynamic;
+
+public interface Human {
+
+	void walk();
+	void talk();
+}
---
+++ src/main/java/com/pratap/designpatterns/structural/proxy/protection/Driver.java
@@ -0,0 +1,19 @@
+package com.pratap.designpatterns.structural.proxy.protection;
+
+public class Driver {
+
+	private int age;
+
+	public Driver(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
