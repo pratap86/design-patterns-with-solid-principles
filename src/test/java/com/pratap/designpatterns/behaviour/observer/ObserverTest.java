9308cb25e764ccc65ebfce382aedc2871dec6d25
pratap86 <narayanpratap86@gmail.com>
2021-05-21 17:25:59 +0530

    add more test cases for observer

--- src/test/java/com/pratap/designpatterns/behaviour/observer/ObserverTest.java
+++ src/test/java/com/pratap/designpatterns/behaviour/observer/ObserverTest.java
@@ -26,4 +26,64 @@ class ObserverTest {
         order.addItem(500);
         assertEquals(20.0, order.getDiscount());
     }
+
+    @Test
+    void testQuantityObserver_When_CountIsUptoFive(){
+        Order order = new Order("100");
+        PriceObserver priceObserver = new PriceObserver();
+        QuantityObserver quantityObserver = new QuantityObserver();
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(200);
+        assertEquals(20.0, order.getDiscount());
+        assertEquals(10.0, order.getShippingCost());
+        System.out.println(order);
+    }
+
+    @Test
+    void testQuantityObserver_When_CountIsUptoTen(){
+        Order order = new Order("100");
+        PriceObserver priceObserver = new PriceObserver();
+        QuantityObserver quantityObserver = new QuantityObserver();
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(200);
+        assertEquals(20.0, order.getDiscount());
+        assertEquals(17.5, order.getShippingCost());
+    }
+
+    @Test
+    void testQuantityObserver_When_CountMoreThanTen(){
+        Order order = new Order("100");
+        PriceObserver priceObserver = new PriceObserver();
+        QuantityObserver quantityObserver = new QuantityObserver();
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(100);
+        order.addItem(200);
+        assertEquals(20.0, order.getDiscount());
+        assertEquals(25.0, order.getShippingCost());
+    }
 }
